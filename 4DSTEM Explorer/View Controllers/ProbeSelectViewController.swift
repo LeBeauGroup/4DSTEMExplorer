ce124d523806900f7aaf20a3ceffda73ce561fab
James LeBeau <jmlebeau@ncsu.edu>
2018-01-16 21:42:35 -0500

    up to v0.75

--- 4DSTEM Explorer/View Controllers/ProbeSelectViewController.swift
+++ 4DSTEM Explorer/View Controllers/ProbeSelectViewController.swift
@@ -9,7 +9,7 @@
 import Cocoa
 import Foundation
 
-class ProbeSelectViewController: NSViewController {
+class ProbeSelectViewController: NSViewController, STEMDataControllerProgressDelegate,NSComboBoxDelegate, NSTextFieldDelegate {
     
     
     @IBOutlet weak var sizeCombo: NSComboBox!
@@ -23,57 +23,101 @@ class ProbeSelectViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sizeCombo.delegate = self
         self.view.window?.initialFirstResponder = loadButton
 
         
         //        progressIndicator.usesThreadedAnimation = true
         NotificationCenter.default.addObserver(self, selector: #selector(updateProgressIndicator(notification:)), name: Notification.Name("updateProgress"), object: nil)
         
-        NotificationCenter.default.addObserver(self, selector: #selector(didFinishLoadingData(notification:)), name: Notification.Name("finishedLoadingData"), object: nil)
-
-    
     }
     
     func selectSizeFromURL(_ url:URL) {
         
-
         let fileName = url.lastPathComponent
         let wh = self.decomposeFilenameString(fileName)
         
-        sizeCombo.stringValue = wh.0 + " x " + wh.1
+        if wh != nil{
+            sizeCombo.stringValue = wh!.0 + " x " + wh!.1
+        }else{
+            sizeCombo.placeholderString = "width x height (pixels)"
+        }
     }
     
-    @IBAction func acceptSize(_ sender: Any) {
-
+    func loadTiff(_ sender: Any) {
         
         sizeCombo.isHidden = true
         loadButton.isEnabled = false
         progressIndicator.isHidden = false
-
+        
         self.title = "Data loading..."
         
-        let wh = self.decomposeComboxString()
-        
-        
-        dataController.imageSize.width = Int(wh.0)!
-        dataController.imageSize.height = Int(wh.1)!
+        self.view.needsDisplay = true
         
         do{
-            try parentController.loadAndFormatData(self)
-        }catch{
+            try dataController.openTIFF(url: dataController.filePath!)
+        }catch FileReadError.invalidTiff{
             
             let alert = NSAlert.init()
             alert.messageText = "Selected image is incompatible.  Please select an EMPAD tiff stack."
-
             alert.runModal()
-    
             self.dismiss(nil)
             
+        }catch{
+            let alert = NSAlert.init()
+            alert.messageText = "Something went wrong with the selected file, please try again."
+            alert.runModal()
+            self.dismiss(nil)
+        }
+    }
+    @IBAction func acceptSize(_ sender: Any) {
+
+        
+        if sizeCombo.stringValue == ""{
+            let alert = NSAlert.init()
+            alert.messageText = "You must enter the width and height of the acquired dataset (final image size) to continue."
+            
+            alert.runModal()
+            
+            return
+        }
+        
+        
+        sizeCombo.isHidden = true
+        loadButton.isEnabled = false
+        progressIndicator.isHidden = false
+    
+        self.title = "Data loading..."
+        
+
+        let wh = self.decomposeComboxString()
+        
+        if wh != nil{
+        
+            dataController.imageSize.width = Int(wh!.0)!
+            dataController.imageSize.height = Int(wh!.1)!
+        }
+        
+        do{
+            try dataController.formatMatrixData()
+        }catch FileReadError.invalidDimensions{
+            sizeCombo.isHidden = false
+            loadButton.isEnabled = true
+            progressIndicator.isHidden = true
+            let alert = NSAlert.init()
+            alert.messageText = "Incorrect dimensions for the file, try again."
+            alert.runModal()
+            
+        }catch{
+            let alert = NSAlert.init()
+            alert.messageText = "Something went wrong with the selected file, please try again."
+            alert.runModal()
+            self.dismiss(nil)
         }
     
     }
     
-    func decomposeFilenameString(_ string:String) -> (String, String){
+    func decomposeFilenameString(_ string:String) -> (String, String)?{
         
         let pat = "(?<=[xy])[0-9]+"
         let regex = try! NSRegularExpression(pattern: pat, options: [])
@@ -90,6 +134,10 @@ class ProbeSelectViewController: NSViewController {
             }
         }
         
+        if matches.count == 0{
+            return nil
+        }
+        
         if matches.count > 1{
             
             if let match = matches.last{
@@ -107,13 +155,17 @@ class ProbeSelectViewController: NSViewController {
         return (width, height)
     }
     
-    func decomposeComboxString() -> (String, String){
-        
+    func decomposeComboxString() -> (String, String)?{
         
         let string = sizeCombo.stringValue
         let pat = "[0-9]+"
         let regex = try! NSRegularExpression(pattern: pat, options: [])
         let matches = regex.matches(in: string, options: [], range: NSRange(location: 0, length: string.count))
+        
+        
+        if matches.count == 0 || matches.count > 2{
+            return nil
+        }
         
         var width:String = ""
         var height:String = ""
@@ -142,6 +194,8 @@ class ProbeSelectViewController: NSViewController {
         
         return (width, height)
     }
+    
+    
 
     @objc func willStartLoadingData(notification:Notification){
         sizeCombo.isHidden = true
@@ -150,7 +204,7 @@ class ProbeSelectViewController: NSViewController {
 
     }
     
-    @objc func didFinishLoadingData(notification:Notification){
+    func didFinishLoadingData(){
     
         self.dismiss(nil)
     }
@@ -166,7 +220,43 @@ class ProbeSelectViewController: NSViewController {
 
     }
     
+
+
+    override func controlTextDidEndEditing(_ obj: Notification) {
+        
+        if let combobox = obj.object as! NSComboBox?{
+            
+            let wh = self.decomposeComboxString()
+            
+            if wh == nil{
+                
+                let alert = NSAlert.init()
+                alert.messageText = "The width and height were entered incorrectly.  Please try again in the proper format."
+                
+                alert.runModal()
+            }
+            
+        }
+    }
     
+    func comboBoxSelectionDidChange(_ notification: Notification) {
+        
+        if let combobox = notification.object as! NSComboBox?{
+         
+            let wh = self.decomposeComboxString()
+            
+            if wh == nil{
+                
+                let alert = NSAlert.init()
+                alert.messageText = "Selected image is incompatible.  Please select an EMPAD tiff stack."
+                
+                alert.runModal()
+            }
+
+            
+        }
+        
+    }
     override func viewDidDisappear() {
         
 
