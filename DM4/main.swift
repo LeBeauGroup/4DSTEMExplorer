66812e31bb8b6458d3ca93f143c6a908101f7175
lebeau <jmlebeau@gmail.com>
2025-07-24 10:11:30 -0400

    dm4 support moving along,  now reading the entire file

---
+++ DM4/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  DM4
+//
+//  Created by James LeBeau on 7/22/25.
+//  Copyright © 2025 The LeBeau Group. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+
+    var expandingTildeInPath: String {
+            return NSString(string: self).expandingTildeInPath
+        }
+
+}
+
+let fileURL = "~/Desktop/test_data/Diffraction SI.dm4".expandingTildeInPath
+
+let url = URL(fileURLWithPath: fileURL)
+print(url.absoluteString)
+let dm4 = try DigitalMicrographReader(fileURL: url)
+
+print(dm4.imageList())
+
+
+
+
+
+
