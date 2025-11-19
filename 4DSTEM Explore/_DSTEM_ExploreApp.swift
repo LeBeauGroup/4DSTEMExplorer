b8fe357b4f864ec24795556a10e719839fb20d02
subangstrom <lebeau@mit.edu>
2025-11-19 13:32:16 -0500

    first commit

---
+++ 4DSTEM Explore/_DSTEM_ExploreApp.swift
@@ -0,0 +1,18 @@
+//
+//  _DSTEM_ExploreApp.swift
+//  4DSTEM Explore
+//
+//  Created by James LeBeau on 11/19/25.
+//  Copyright © 2025 The LeBeau Group. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct _DSTEM_ExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 4DSTEM ExploreTests/_DSTEM_ExploreTests.swift
@@ -0,0 +1,18 @@
+//
+//  _DSTEM_ExploreTests.swift
+//  4DSTEM ExploreTests
+//
+//  Created by James LeBeau on 11/19/25.
+//  Copyright © 2025 The LeBeau Group. All rights reserved.
+//
+
+import Testing
+@testable import _DSTEM_Explore
+
+struct _DSTEM_ExploreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
